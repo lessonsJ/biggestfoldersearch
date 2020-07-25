a576f04b282b414d1107fa387228cff0446081c2
SomeName <redddjen@gmail.com>
2020-07-25 15:47:02 +0300

    Двигаем текст

--- src/Main.java
+++ src/Main.java
@@ -1,66 +1,25 @@
 import java.io.File;
 import java.util.concurrent.ForkJoinPool;
 
-public class Main {
-    private static final int STEP = 1024;
-    private static final int ONE = 1;
+public class Main
+{
 
-    public static void main(String[] args){
-        String folderPath = "/Project/Исходная папка/";
+    public static void main(String[] args)
+    {
+        String folderPath = "D:/Project/Исходная папка/";
         File file = new File(folderPath);
+        Node root = new Node(file);
 
-        FolderSizeCalc calc = new FolderSizeCalc(file);
+        long start = System.currentTimeMillis();
+
+        FolderSizeCalculator calculator =
+                new FolderSizeCalculator(root);
         ForkJoinPool pool = new ForkJoinPool();
-        long size = pool.invoke(calc);
-        System.out.println("Размер папки/файла = " + getHumanReadableSize(size));
-        System.out.println(getSizeFromHumanReadable(getHumanReadableSize(size)));
-    }
+        pool.invoke(calculator);
 
-    public static String getHumanReadableSize(long size){
-        long kb = size / STEP;
-        long mb = kb / STEP;
-        long gb = mb / STEP;
-        long tb = gb / STEP;
+        System.out.println(root);
 
-        if (kb > ONE) {
-            return(kb + " Kb");
-        }
-
-        if (mb > ONE) {
-            return(mb + " Mb");
-        }
-
-        if (gb > ONE) {
-            return (mb + " Gb");
-        }
-
-        if (tb > ONE) {
-            return (mb + " Tb");
-        }
-        return Long.toString(size); // B
-    }
-
-    public static long getSizeFromHumanReadable(String size){
-
-        long getSize = Long.parseLong(size.replaceAll("\\D+",""));
-        String getName = size.substring(size.length() - 2).trim();
-        System.out.println(getName + "  " + getSize);
-        if (getName.equals("B")){
-            return getSize;
-        }
-        if (getName.equals("Kb")){
-            return getSize * STEP;
-        }
-        if (getName.equals("Gb")){
-            return getSize * STEP * STEP;
-        }
-        if (getName.equals("Tb")){
-            return getSize * STEP * STEP * STEP;
-        }
-        return 0;
-    }
-
-    private static void message(String messageName) {
-        System.out.println(messageName);
+        long duration = System.currentTimeMillis() - start;
+        System.out.println(duration + " ms");
     }
 }
