096dac6aa666a4377f9e2d681bc592143951866c
SomeName <redddjen@gmail.com>
2020-07-25 14:20:21 +0300

    Размерность папки

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,26 @@
+import java.io.File;
+import java.util.concurrent.ForkJoinPool;
+
 public class Main {
+    public static void main(String[] args){
+        String folderPath = "/Project/Исходная папка/";
+        File file = new File(folderPath);
+
+        FolderSizeCalc calc = new FolderSizeCalc(file);
+        ForkJoinPool pool = new ForkJoinPool();
+        long size = pool.invoke(calc);
+        System.out.println(size);
+    }
+
+    public static long getFolderSize(File folder){
+        if(folder.isFile()){
+            return folder.length();
+        }
+        long sum = 0;
+        File[] files = folder.listFiles();
+        for(File file : files){
+            sum += getFolderSize(file);
+        }
+        return sum;
+    }
 }
